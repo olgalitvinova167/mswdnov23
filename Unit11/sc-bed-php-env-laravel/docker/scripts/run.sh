ef7a35bd59a9a9296da6be5cfca73d9181899071
Olga Litvinova <litvinova.olga167@gmail.com>
2024-02-19 20:08:37 +0100

    Unit 11 complete.

---
+++ Unit11/sc-bed-php-env-laravel/docker/scripts/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /var/www/public
+
+php-fpm -F
---
+++ Unit11/sc-bed-php-env-laravel/sc-bed-productdb/app/Http/Controllers/ProductController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('products.index', [
+            'products' => Product::latest()->paginate(3)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductRequest $request): RedirectResponse
+    {
+        Product::create($request->all());
+        return redirect()->route('products.index')->withSuccess('New product added successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return view('products.show', [
+            'product' => $product
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        return view('products.edit', [
+            'product' => $product
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductRequest $request, Product $product): RedirectResponse
+    {
+        $product->update($request->all());
+        return redirect()->back()->withSuccess('Product updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product): RedirectResponse
+    {
+        $product->delete();
+        return redirect()->route('products.index')->withSuccess('Product deleted');
+    }
+}
